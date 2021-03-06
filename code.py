0e8861c83e6740882356d6e4d7059ce2045241ba
Mukhailo <savcukmihajlo@gmail.com>
2021-03-06 19:31:03 +0200

    End

--- code.py
+++ code.py
@@ -1,6 +1,20 @@
 from colorama import *
 init()
+
+def what1():
+    print(f"""{Fore.CYAN}
+        1.Encrypt
+        2.Decrypt
+        """)
+
+def what2():
+    print(f"""{Fore.CYAN}
+        1.Caesar
+        2.Gronsfeld
+    """)
+
 # logo
+
 def logo():
     print(f"""{Fore.GREEN}
         ████████╗███████╗██╗░░██╗████████╗███████╗███╗░░██╗░█████╗░██████╗░██╗░░░██╗██████╗░████████╗
@@ -10,24 +24,14 @@ def logo():
         ░░░██║░░░███████╗██╔╝╚██╗░░░██║░░░███████╗██║░╚███║╚█████╔╝██║░░██║░░░██║░░░██║░░░░░░░░██║░░░
         ░░░╚═╝░░░╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚══════╝╚═╝░░╚══╝░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░░░░░░░╚═╝░░░
         """)
-    print("""
-        1.Encrypt
-        2.Decrypt
-        """)
-# logo output
-logo()
-
-namber = int(input('Enter namber: '))
 
 def values():
     global offset, stri
     stri = (input('Enter text: '))
     offset = int(input('Enter value: '))
 
-
-
-def encrypt(offset, stri):
-# creat a list of encrypted words.
+def Caesar_encrypt(offset, stri):
+    # creat a list of encrypted words.
     stri = stri.split()
 
     # creat a list to hold decrypted words.
@@ -42,9 +46,8 @@ def encrypt(offset, stri):
     sentence = ' '.join(res)
     print(sentence)
 
-
-def decrypt(offset, stri):
-# creat a list of encrypted words.
+def Caesar_decrypt(offset, stri):
+    # creat a list of encrypted words.
     stri = stri.split()
     # creat a list to hold decrypted words.
     res = []
@@ -57,13 +60,72 @@ def decrypt(offset, stri):
     # join each word in the sentence list back together by a space.
     sentence = ' '.join(res)
     print(sentence)
-# Choise encrypt or decrypt
+
+def Gronsfeld_encrypt(stri, offset):
+    # creat a list of encrypted words.
+    stri = stri.split()
+    # creat a list to hold decrypted words.
+    res = []
+    for word in stri:
+        cipher_ords = [ord(x) for x in word]
+        for r in offset:
+            c = int(r)
+        plaintext_ords = [o - c for o in cipher_ords]
+        plaintext_chars = [chr(i) for i in plaintext_ords]
+        plaintext = ''.join(plaintext_chars)
+        res.append(plaintext)
+    # join each word in the sentence list back together by a space.
+    sentence = ' '.join(res)
+    print(sentence)
+
+def Gronsfeld_decrypt(stri, offset):
+    # creat a list of encrypted words.
+    stri = stri.split()
+    # creat a list to hold decrypted words.
+    res = []
+
+    for word in stri:
+        cipher_ords = [ord(x) for x in word]
+        for r in offset:
+            c = int(r)
+            plaintext_ords = [o - c for o in cipher_ords]
+    plaintext_chars = [chr(i) for i in plaintext_ords]
+    plaintext = ''.join(plaintext_chars)
+    res.append(plaintext)
+    # join each word in the sentence list back together by a space.
+    sentence = ' '.join(res)
+    print(sentence)
+
+def text_encrypt():
+    logo()
+    what2()
+    namber = int(input('Enter namber: '))
+    if namber == 1:
+        values()
+        Caesar_encrypt(offset,stri)
+    elif namber == 2:
+        values()
+        Gronsfeld_encrypt(offset,stri)
+
+def text_decrypt():
+    logo()
+    what2()
+    namber = int(input('Enter namber: '))
+    if namber == 1:
+        values()
+        Caesar_decrypt(offset,stri)
+    elif namber == 2:
+        values()
+        Gronsfeld_decrypt(offset,stri)
+
+logo()
+what1()
+namber = int(input('Enter namber: '))
+
 if namber == 1:
-    values()
-    encrypt(offset,stri)
+    text_encrypt()
 elif namber == 2:
-    values()
-    decrypt(offset, stri)
+    text_decrypt()
 
 
 
--- test.py
+++ test.py
@@ -1,12 +1,17 @@
-offset = int(input('Offset '))
-    str = input('String ')
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    res = []
-    ln = len(alphabet)
-    n = '.\/[]{}()=-.,;:\'"1234567890~!`@#$%^&*<>?|=+_- '
-    for l in str:
-        if not l in n:
-            res.append(alphabet[(alphabet.find(l)+offset)%ln])
-        else:
-            res.append(l)
-    print(''.join(res))
+stri = input('Enter text: ')
+offset = input('Enter values: ')
+# creat a list of encrypted words.
+stri = stri.split()
+# creat a list to hold decrypted words.
+res = []
+for word in stri:
+    cipher_ords = [ord(x) for x in word]
+    for r in offset:
+        c = int(r)
+    plaintext_ords = [o - c for o in cipher_ords]
+    plaintext_chars = [chr(i) for i in plaintext_ords]
+    plaintext = ''.join(plaintext_chars)
+    res.append(plaintext)
+    # join each word in the sentence list back together by a space.
+sentence = ' '.join(res)
+print(sentence)
